bcff54a62581d866742d6f3519792b501ef43359
Jasmin <takamchristian2@gmail.com>
2023-01-08 20:37:16 +0100

    fix login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,14 @@
 
 import { Page } from "@playwright/test";
-import data from "./datas/connection_data.json";
+import data from "./data_input.json";
 
 
 async function login(page: Page) {
   await page.goto("https://ztrain-web.vercel.app/auth/login");
   await page.getByPlaceholder("Email").click();
-  await page.getByPlaceholder("Email").fill(data.email);
+  await page.getByPlaceholder("Email").fill(data.login.email);
   await page.getByPlaceholder("Mot de passe").click();
-  await page.getByPlaceholder("Mot de passe").fill(data.mot_de_passe);
+  await page.getByPlaceholder("Mot de passe").fill(data.login.mot_de_passe);
   await page.locator("#btn_login").click();
 
   page.pause;
